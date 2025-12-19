3779ca9b6a840002e775a45e29a81f521f760d08
Rinto <rintorinto2000111@gmail.com>
2025-12-19 21:36:39 +0900

    UI: adjust map buttons and layer toggle

--- components/Map.jsx
+++ components/Map.jsx
@@ -2,13 +2,13 @@
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-export default function Map({ children, tileLayerUrl }) {
+export default function Map({ children, isSatellite }) {
+  const tileLayerUrl = isSatellite
+    ? 'https://cyberjapandata.gsi.go.jp/xyz/ort/{z}/{x}/{y}.jpg'
+    : 'https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png';
+
   return (
-    <MapContainer
-      center={[35.5117, 139.4754]}
-      zoom={15}
-      style={{ height: '100%', width: '100%' }}
-    >
+    <MapContainer center={[35.5117, 139.4754]} zoom={15} style={{ height: '100%', width: '100%' }}>
       <TileLayer
         attribution='&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>'
         url={tileLayerUrl}
@@ -17,4 +17,3 @@ export default function Map({ children, tileLayerUrl }) {
     </MapContainer>
   );
 }
-
