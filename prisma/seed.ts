d44074e284bd9b095c57a6a7c1118997f990cb09
labsukukakeKT <naks.85.lab@gmail.com>
2025-12-20 23:45:45 +0900

    seed list added

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,28 +2,36 @@ import { PrismaClient } from '@prisma/client'
 import { places } from './seed_placedata'
 const prisma = new PrismaClient()
 
+const CHANGE_PLACE_DB = true;
+const CHANGE_USER_DB = false;
+
 async function main() {
-  for (const p of places) {
-    await prisma.place_DB.upsert({
-      where: { place_id: p.place_id },
-      update: {
-        lat: p.lat,
-        lng: p.lng,
-        place_description: p.place_description,
-        place_photo_url: p.place_photo_url,
-        place_era_start: p.place_era_start,
-        place_era_end: p.place_era_end,
-      },
-      create: {
-        place_name: p.place_name,
-        lat: p.lat,
-        lng: p.lng,
-        place_description: p.place_description,
-        place_photo_url: p.place_photo_url,
-        place_era_start: p.place_era_start,
-        place_era_end: p.place_era_end,
-      },
-    })
+  // 依存関係のある子テーブルから順に削除
+  if (CHANGE_PLACE_DB) {
+    await prisma.place_DB.deleteMany()
+
+    for (const p of places) {
+      await prisma.place_DB.upsert({
+        where: { place_id: p.place_id },
+        update: {
+          lat: p.lat,
+          lng: p.lng,
+          place_description: p.place_description,
+          place_photo_url: p.place_photo_url,
+          place_era_start: p.place_era_start,
+          place_era_end: p.place_era_end,
+        },
+        create: {
+          place_name: p.place_name,
+          lat: p.lat,
+          lng: p.lng,
+          place_description: p.place_description,
+          place_photo_url: p.place_photo_url,
+          place_era_start: p.place_era_start,
+          place_era_end: p.place_era_end,
+        },
+      })
+    }
   }
 
 
@@ -38,22 +46,26 @@ async function main() {
     },
   ]
 
-  for (const u of users) {
-    await prisma.user_DB.upsert({
-      where: { user_id: u.user_id },
-      update: {
-        user_name: u.user_name,
-        user_description: u.user_description,
-        email: u.email,
-        password: u.password,
-      },
-      create: {
-        user_name: u.user_name,
-        user_description: u.user_description,
-        email: u.email,
-        password: u.password,
-      },
-    })
+
+  if (CHANGE_USER_DB) {
+    await prisma.user_DB.deleteMany()
+    for (const u of users) {
+      await prisma.user_DB.upsert({
+        where: { user_id: u.user_id },
+        update: {
+          user_name: u.user_name,
+          user_description: u.user_description,
+          email: u.email,
+          password: u.password,
+        },
+        create: {
+          user_name: u.user_name,
+          user_description: u.user_description,
+          email: u.email,
+          password: u.password,
+        },
+      })
+    }
   }
 }
 
